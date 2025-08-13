537eb95fd93c114a25ba909a1aebbdb9fae70ada
willkorku-eng <willkorku@gmail.com>
2025-08-13 13:24:58 +0000

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client";
 import { CssBaseline } from "@mui/material";
 import App from "./App";
 
-import "@fullcalendar/common/main.css";
-import "@fullcalendar/daygrid/main.css";
-import "@fullcalendar/timegrid/main.css";
+import "@fullcalendar/common";
+import "@fullcalendar/daygrid";
+import "@fullcalendar/timegrid";
 import "tippy.js/dist/tippy.css";
 
 createRoot(document.getElementById("root")!).render(
